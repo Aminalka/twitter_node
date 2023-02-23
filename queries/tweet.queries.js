6c0e4a231d5c1ea5ae659eacae2b1b442d9bfa0d
Amina lkabche <aminalkabche@gmail.com>
2023-02-24 00:49:51 +0100

    feat:mose en place du MVC

---
+++ queries/tweet.queries.js
@@ -0,0 +1,10 @@
+const Tweet=require("../database/models/Tweet.model")
+
+exports.creatNewTweet=(body) => {
+    const newTweet=new Tweet(body);
+    return newTweet.save();
+}
+
+exports.findAllTweets=() => {
+    return Tweet.find({}).exec();
+}
