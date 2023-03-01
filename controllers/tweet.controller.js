0fd25ccf9be43e34d33375fa106644f585128a46
Amina lkabche <aminalkabche@gmail.com>
2023-03-01 18:07:41 +0100

    feat: Modification d'un tweet

--- controllers/tweet.controller.js
+++ controllers/tweet.controller.js
@@ -1,5 +1,5 @@
 
-const {createNewTweet,findAllTweets,findTweetAndDelete}=require('../queries/tweet.queries');
+const {createNewTweet,findAllTweets,findTweetAndDelete, findTweetById, findTweetAndUpdate}=require('../queries/tweet.queries');
 
 
 exports.createTweet= async(req, res, next) => {
@@ -42,3 +42,28 @@ exports.deleteTweet = async (req, res, next) => {
         next(error);
     }
 }
+
+exports.displayTweet= async (req, res, next) => {
+    try {
+        const tweetId=req.params.tweetId;
+        const tweet=await findTweetById(tweetId);
+        res.render('tweets/tweet-edit', {tweet, isAuthenticated:req.isAuthenticated,currentUser:req.user})
+    } catch (error) {
+        next(error)
+        
+    }
+}
+exports.updateTweet= async(req, res, next) => {
+    try {
+        const tweetId=req.params.tweetId;
+        const body=req.body;
+        await findTweetAndUpdate(tweetId, body);
+        res.redirect('/');
+
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}
+
--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -1,4 +1,4 @@
-const { createTweet, deleteTweet } = require('../controllers/tweet.controller');
+const { createTweet, deleteTweet, displayTweet, updateTweet } = require('../controllers/tweet.controller');
 
 const router=require('express').Router();
 
@@ -6,6 +6,8 @@ const router=require('express').Router();
 
 router.post('/new',createTweet );   //creation d un tweet
 router.get('/delete/:tweetId',deleteTweet); //supprimer un tweet
+router.get('/edit/:tweetId',displayTweet); // affiche le tweet pour la modification
+router.post('/edit/:tweetId',updateTweet); // Mise a jour du twwet
 
 
 
