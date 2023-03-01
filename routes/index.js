f7606a3609ba5c6e6a06bafb15b23637e65b3485
Amina lkabche <aminalkabche@gmail.com>
2023-03-01 21:16:57 +0100

    feat:middleware ensureAuthenticated

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ const userRoute=require('./user.routes');
 const authRoute = require('./auth.routes');
 
 const { tweetList } = require('../controllers/tweet.controller');
+const { ensureAuthenticated } = require('../config/security.config');
 
-router.use('/tweet',tweetRoute);
+router.use('/tweet',ensureAuthenticated, tweetRoute);
 router.use('/user',userRoute);
 router.use('/auth',authRoute)
 
